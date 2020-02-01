ea90e5dcfae1928a41342c228522cc6fd9ef8551
Recleph Mere <mere.recleph@gmail.com>
2020-01-31 22:34:29 -0500

    Finished the Encrypter

---
+++ Program Assignment 2/mere_p1/mere_p1/src/app/App.java
@@ -0,0 +1,17 @@
+package app;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        // Get user input
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter your 4 digit integer: ");
+        String userString = input.next();
+
+        //Encrypt user input
+
+        //Decrypt user input
+
+
+    }
+}
