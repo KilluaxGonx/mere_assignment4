2be579367e3df474b3599702c41cfac1127f8045
Recleph Mere <mere.recleph@gmail.com>
2020-02-01 22:40:04 -0500

    Fixing file name

--- Program Assignment 2/mere_p1/src/app/App.java
+++ Program Assignment 2/mere_p1/src/app/App.java
@@ -1,6 +1,8 @@
 /** Author: Recleph Mere
  *  Date: 02/1/2020
  *  Description: This program encrypts and decrypts 4 digit integers using a specific algorithm.
+ *  NOTE: I was not sure if we were free to create our own classes outside the ones mentioned on the rubric.
+ *        Many of the functions in main I wanted to create seperate classes for.  
 */
 package app;
 import java.util.Scanner;
